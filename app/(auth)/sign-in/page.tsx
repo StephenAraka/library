9f1110909ac10bbaff950965749a07f12e04161a
StephenAraka <stevenaraka@gmail.com>
2025-02-19 21:09:27 +0300

    Create reusable auth form component

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,18 @@
+'use client';
+
+import AuthForm from '@/components/AuthForm';
+import { signInSchema } from '@/lib/validations';
 import React from 'react';
 
 const Page = () => {
-  return <div>Sign In Form</div>;
+  return (
+    <AuthForm
+      type="SIGN_IN"
+      schema={signInSchema}
+      defaultValues={{ email: '', password: '' }}
+      onSubmit={() => {}}
+    />
+  );
 };
 
 export default Page;
