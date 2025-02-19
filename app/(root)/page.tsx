0f174ec15e7a6332611b6bbb72430b17cfcd23e4
StephenAraka <stevenaraka@gmail.com>
2025-02-19 11:02:32 +0300

    Create homepage layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,14 @@
+import BookList from '@/components/BookList';
+import BookOverview from '@/components/BookOverview';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 const Home = () => {
   return (
     <>
-      <Button>Click Me</Button>
+      <BookOverview />
+
+      <BookList />
     </>
   );
 };
