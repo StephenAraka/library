7b3433127481b29ec55c221ec2b801f8d7bad52b
StephenAraka <stevenaraka@gmail.com>
2025-02-19 21:25:24 +0300

    Show dynamic field names in form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,6 +23,8 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import Link from 'next/link';
+import { FIELD_NAMES } from '@/constants';
 
 // T means its generic - used when we dont know what the props may look like
 interface Props<T extends FieldValues> {
@@ -38,6 +40,8 @@ const AuthForm = <T extends FieldValues>({
   defaultValues,
   onSubmit,
 }: Props<T>) => {
+  const isSignIn = type === 'SIGN_IN';
+
   const form: UseFormReturn<T> = useForm({
     resolver: zodResolver(schema),
     defaultValues: defaultValues as DefaultValues<T>,
@@ -46,27 +50,59 @@ const AuthForm = <T extends FieldValues>({
   const handleSubmit: SubmitHandler<T> = async (data) => {};
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-semibold text-white">
+        {isSignIn ? 'Welcome back to BookWise' : 'Create your library account'}
+      </h1>
+
+      <p className="text-light-100">
+        {isSignIn
+          ? 'Access the vast collection of resources, and stay updated'
+          : 'Please complete all fields and upload a valid university ID to gain access to the library'}
+      </p>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="space-y-6 w-full"
+        >
+          {Object.keys(defaultValues).map((field) => (
+            <FormField
+              key={field}
+              control={form.control}
+              name={field as Path<T>}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="capitalize">
+                    {FIELD_NAMES[field.name as keyof typeof FIELD_NAMES]}
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+
+      <p className="text-center text-base font-medium">
+        {isSignIn ? 'New to BookWise? ' : 'Already have ab account? '}
+
+        <Link
+          href={isSignIn ? '/sign-up' : '/sign-in'}
+          className="font-bold text-primary"
+        >
+          {isSignIn ? 'Create an account' : 'Sign in'}
+        </Link>
+      </p>
+    </div>
   );
 };
 
